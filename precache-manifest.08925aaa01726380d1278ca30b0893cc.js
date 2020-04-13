878b6ecfeddd50d2576aa5a409b04e70e8fbea01
Tariq Khan <digitalgnomes@gmail.com>
2020-04-12 23:57:30 -0700

    Updates

---
+++ precache-manifest.08925aaa01726380d1278ca30b0893cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc3dcd4c98bb4b6640b9e47ab17370e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5bedcfd39358234d1e96",
+    "url": "/monsters-rolodex/static/css/main.12e82959.chunk.css"
+  },
+  {
+    "revision": "9da331170154af3e6af1",
+    "url": "/monsters-rolodex/static/js/2.3e3b13d7.chunk.js"
+  },
+  {
+    "revision": "5bedcfd39358234d1e96",
+    "url": "/monsters-rolodex/static/js/main.dcab0251.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
